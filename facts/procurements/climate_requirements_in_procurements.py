8cb898907ff1295bd6271fc77722c195c8e0368c
elvbom <ecdb@protonmail.com>
2025-08-13 13:26:03 +0200

    update climate data json file and add missing nur data

--- facts/procurements/climate_requirements_in_procurements.py
+++ facts/procurements/climate_requirements_in_procurements.py
@@ -5,32 +5,36 @@ import pandas as pd
 from helpers import clean_kommun
 
 
-PATH_GREENPEACE_DATA = 'facts/procurements/Klimatkrav offentlig upphandling kartläggning.xlsx'
-
-RELATIVE_PATH_NUR_DATA = os.path.join(
-    os.path.dirname(__file__),
-    "../../../public/data/procurements/NUE2022_DATA_2023-12-20.xlsx"
+PATH_GREENPEACE_DATA = (
+    "facts/procurements/Klimatkrav offentlig upphandling kartläggning.xlsx"
 )
-NORMALIZED_PATH_NUR_DATA = os.path.normpath(RELATIVE_PATH_NUR_DATA)
+PATH_NUR_DATA = "facts/procurements/NUE2022_DATA_2023-12-20.xlsx"
+
 
 def get_greenpeace_data():
     df_greenpeace = pd.read_excel(PATH_GREENPEACE_DATA)
-    df_greenpeace['procurementLink'] = df_greenpeace['Länk\nKlimat\nkrav']
-    df_greenpeace['procurementLink'] = df_greenpeace['procurementLink'].fillna('')
-    return df_greenpeace[['Kommun', 'procurementLink']]
+    df_greenpeace["procurementLink"] = df_greenpeace["Länk\nKlimat\nkrav"]
+    df_greenpeace["procurementLink"] = df_greenpeace["procurementLink"].fillna("")
+    return df_greenpeace[["Kommun", "procurementLink"]]
+
 
 def get_nur_data():
-    df_nur = pd.read_excel(NORMALIZED_PATH_NUR_DATA)
-    df_nur['Kommun'] = df_nur['ORG_NAMN'].apply(clean_kommun)
-    return df_nur[['Kommun', 'BINÄRT_UTFALL']]
+    df_nur = pd.read_excel(PATH_NUR_DATA)
+    df_nur["Kommun"] = df_nur["ORG_NAMN"].apply(clean_kommun)
+    return df_nur[["Kommun", "BINÄRT_UTFALL"]]
+
 
 def calculate_procurement_score(df_greenpeace, df_nur):
-    df_merge = pd.merge(df_greenpeace, df_nur, on='Kommun', how='left')
-    df_merge['procurementScore'] = df_merge.apply(
-        lambda row: 2 if row['procurementLink'] != '' else 1 if row['BINÄRT_UTFALL'] > 0 else 0, axis=1
-        )
-    df_fill = df_merge.fillna(0) 
-    return df_fill[['Kommun', 'procurementLink', 'procurementScore']]
+    df_merge = pd.merge(df_greenpeace, df_nur, on="Kommun", how="left")
+    df_merge["procurementScore"] = df_merge.apply(
+        lambda row: (
+            2 if row["procurementLink"] != "" else 1 if row["BINÄRT_UTFALL"] > 0 else 0
+        ),
+        axis=1,
+    )
+    df_fill = df_merge.fillna(0)
+    return df_fill[["Kommun", "procurementLink", "procurementScore"]]
+
 
 def get_procurement_data():
     """
