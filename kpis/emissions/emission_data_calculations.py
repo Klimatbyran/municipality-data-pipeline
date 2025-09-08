4375dd8e20ce578bae6ef7cb2b1ebf85b21c62d2
elvbom <ecdb@protonmail.com>
2025-09-08 20:20:02 +0200

    correct generation of total trend

--- kpis/emissions/emission_data_calculations.py
+++ kpis/emissions/emission_data_calculations.py
@@ -93,7 +93,11 @@ def calculate_meets_paris_goal(total_trend, total_carbon_law_path):
     """
     Calculate if the municipality meets the Paris goal.
     """
-    return total_trend <= total_carbon_law_path
+    print("--------------------------------")
+    print(total_trend, total_carbon_law_path)
+    result = total_trend <= total_carbon_law_path
+    print(result)
+    return result
 
 
 def emission_calculations(df):
@@ -113,22 +117,27 @@ def emission_calculations(df):
 
     df_trend_and_approximated = calculate_trend(df_cem, CURRENT_YEAR, END_YEAR)
 
-    df_trend_and_approximated["totalTrend"] = calculate_total_trend(
-        df_trend_and_approximated
-    )
-
-    df_trend_and_approximated["meetsParisGoal"] = calculate_meets_paris_goal(
-        df_trend_and_approximated["totalTrend"],
-        df_trend_and_approximated["totalCarbonLawPath"],
+    df_trend_and_approximated["totalTrend"] = df_trend_and_approximated.apply(
+        lambda row: row[[col for col in row.index if "trend_" in str(col)]].sum(),
+        axis=1,
     )
 
     df_historical_change_percent = calculate_historical_change_percent(
         df_trend_and_approximated, LAST_YEAR_WITH_SMHI_DATA
     )
 
-    return calculate_carbon_law_total(
+    df_carbon_law = calculate_carbon_law_total(
         df_historical_change_percent,
         CURRENT_YEAR,
         END_YEAR,
         CARBON_LAW_REDUCTION_RATE,
     )
+
+    df_carbon_law["meetsParisGoal"] = df_carbon_law.apply(
+        lambda row: calculate_meets_paris_goal(
+            row["totalTrend"], row["totalCarbonLawPath"]
+        ),
+        axis=1,
+    )
+
+    return df_carbon_law
