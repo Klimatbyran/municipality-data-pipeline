cd209ad03f99f0327d372dc22c213f2575ea8820
Elvira <elvbom@users.noreply.github.com>
2025-09-08 20:21:20 +0200

    [Fix] update meets paris goal (#45)
    * correct paris goal test and calc
    
    * source meets paris goal directly from col
    
    * correct generation of total trend
    
    * update municipality climate data

--- kpis/emissions/emission_data_calculations.py
+++ kpis/emissions/emission_data_calculations.py
@@ -89,6 +89,17 @@ def calculate_historical_change_percent(df, last_year_in_range):
     return df
 
 
+def calculate_meets_paris_goal(total_trend, total_carbon_law_path):
+    """
+    Calculate if the municipality meets the Paris goal.
+    """
+    print("--------------------------------")
+    print(total_trend, total_carbon_law_path)
+    result = total_trend <= total_carbon_law_path
+    print(result)
+    return result
+
+
 def emission_calculations(df):
     """
     Perform emission calculations based on the given dataframe.
@@ -106,17 +117,27 @@ def emission_calculations(df):
 
     df_trend_and_approximated = calculate_trend(df_cem, CURRENT_YEAR, END_YEAR)
 
-    df_trend_and_approximated["totalTrend"] = calculate_total_trend(
-        df_trend_and_approximated
+    df_trend_and_approximated["totalTrend"] = df_trend_and_approximated.apply(
+        lambda row: row[[col for col in row.index if "trend_" in str(col)]].sum(),
+        axis=1,
     )
 
     df_historical_change_percent = calculate_historical_change_percent(
         df_trend_and_approximated, LAST_YEAR_WITH_SMHI_DATA
     )
 
-    return calculate_carbon_law_total(
+    df_carbon_law = calculate_carbon_law_total(
         df_historical_change_percent,
         CURRENT_YEAR,
         END_YEAR,
         CARBON_LAW_REDUCTION_RATE,
     )
+
+    df_carbon_law["meetsParisGoal"] = df_carbon_law.apply(
+        lambda row: calculate_meets_paris_goal(
+            row["totalTrend"], row["totalCarbonLawPath"]
+        ),
+        axis=1,
+    )
+
+    return df_carbon_law
