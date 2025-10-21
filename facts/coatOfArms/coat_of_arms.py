acb7403ea73ccc278acbcf32a4688616fd7b6d48
JohnnyRico <156791432+CarlWiren90@users.noreply.github.com>
2025-10-21 11:14:30 +0200

    Update facts/coatOfArms/coat_of_arms.py
    Co-authored-by: Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>

--- facts/coatOfArms/coat_of_arms.py
+++ facts/coatOfArms/coat_of_arms.py
@@ -4,7 +4,6 @@ import json
 from urllib.parse import quote
 
 
-
 """ Måste skapa en check som först kollar t.ex "bollnäs" wikiId om den inte har en coat of arms, kolla bollnäs municipality-wikiId. M """
 
 
