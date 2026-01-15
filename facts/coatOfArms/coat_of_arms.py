60da6b7339fd2ac3836e849bba309db35f273bac
elvbom <ecdb@protonmail.com>
2026-01-15 10:33:46 +0100

    minor fixes in coat of arms test

--- facts/coatOfArms/coat_of_arms.py
+++ facts/coatOfArms/coat_of_arms.py
@@ -1,3 +1,4 @@
+"""Module for retrieving municipality coat of arms images from Wikidata."""
 from urllib.parse import quote
 import requests
 
@@ -20,18 +21,19 @@ def get_coat_of_arms(municipality_name):
 
     if not wiki_ids:
         return None
-   
-    
     headers = {"User-Agent": "KlimatkollenFetcher/1.0 (contact: hej@klimatkollen.se)"}
     coat_of_arms_url = None
-   
-    for id in wiki_ids:
-        url = f'https://www.wikidata.org/w/api.php?action=wbgetentities&ids={id}&props=claims&format=json'
+
+    for wiki_id in wiki_ids:
+        url = (
+            f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids="
+            f"{wiki_id}&props=claims&format=json"
+        )
         res = requests.get(url, headers=headers, timeout=30)
- 
+
         try:
             response = res.json()
-            claims = response["entities"][id]["claims"]
+            claims = response["entities"][wiki_id]["claims"]
 
             p94 = claims.get("P94")
             p154 = claims.get("P154")
@@ -45,10 +47,12 @@ def get_coat_of_arms(municipality_name):
                     if "datavalue" in snak:
                         filename = snak["datavalue"]["value"].replace(" ", "_")
                         break
-                
+
                 if filename and isinstance(filename, str):
-                    print(filename)
-                    url = f"https://commons.wikimedia.org/wiki/Special:Redirect/file/{quote(filename)}"
+                    url = (
+                        f"https://commons.wikimedia.org/wiki/Special:Redirect/file/"
+                        f"{quote(filename)}"
+                    )
                     res = requests.get(url, headers=headers, allow_redirects=True, timeout=30)
                     coat_of_arms_url = res.url
 
@@ -61,9 +65,12 @@ def get_coat_of_arms(municipality_name):
                         if "datavalue" in snak:
                             filename = snak["datavalue"]["value"].replace(" ", "_")
                             break
-                    
+
                     if filename and isinstance(filename, str):
-                        url = f"https://commons.wikimedia.org/wiki/Special:Redirect/file/{quote(filename)}"
+                        url = (
+                            f"https://commons.wikimedia.org/wiki/Special:Redirect/file/"
+                            f"{quote(filename)}"
+                        )
                         res = requests.get(url, headers=headers, allow_redirects=True, timeout=30)
                         coat_of_arms_url = res.url
 
@@ -78,6 +85,7 @@ def get_coat_of_arms(municipality_name):
 
 
 def get_municipality_wiki_id(municipality_name):
+    """Get municipality wiki ID from Wikidata."""
     url = "https://www.wikidata.org/w/api.php"
     params = {
         "action": "wbsearchentities",
@@ -90,18 +98,21 @@ def get_municipality_wiki_id(municipality_name):
     }
 
     res = requests.get(url, params=params, headers=headers, timeout=30)
-  
+
     try:
         response = res.json()
         search_results = response.get("search", [])
         if not search_results:
-            return [] 
+            return []
 
         wiki_ids = [search_results[0].get("id")]
 
         if len(search_results) > 1:
             for municipality in search_results:
-                if municipality_name + " Municipality" in municipality["label"]:
+                if (
+                    f"{municipality_name} Municipality"
+                    in municipality["label"]
+                ):
                     valid_id = municipality.get("id")
                     if valid_id not in wiki_ids:
                         wiki_ids.append(valid_id)
