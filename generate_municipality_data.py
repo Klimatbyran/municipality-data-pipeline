e5231be3d3beff3663f8c88a61baf51a4c0a3506
Elvira <elvbom@users.noreply.github.com>
2026-01-16 13:39:53 +0100

    correct trend emissions slope placement and update data (#82)

--- generate_municipality_data.py
+++ generate_municipality_data.py
@@ -100,6 +100,7 @@ def series_to_dict(
             for year in approximated_columns
         },
         "trend": {year.replace("trend_", ""): row[year] for year in trend_columns},
+        "emissionsSlope": row["trend_emissions_slope"],
         "historicalEmissionChangePercent": row["historicalEmissionChangePercent"],
         "hitNetZero": row["hit_net_zero"],
         "electricCarChangePercent": row["evChangeRate"],
@@ -127,7 +128,7 @@ def df_to_dict(input_df: pd.DataFrame, num_decimals: int) -> dict:
     trend_columns = [
         col
         for col in input_df.columns
-        if "trend_" in str(col) and "coefficient" not in str(col)
+        if "trend_" in str(col) and "coefficient" not in str(col) and "slope" not in str(col)
     ]
 
     rounded_df = input_df.round(num_decimals)
